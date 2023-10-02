cd95a3d224ef9294d56a582633a218c58860e522
Viper <viper7zu7@gmail.com>
2023-10-02 17:14:57 -0300

    a

--- src/inputs/Keyboardinputs.java
+++ src/inputs/Keyboardinputs.java
@@ -1,2 +1,51 @@
-package inputs;public class Keyboardinputs {
+package inputs;
+
+import main.GamePanel;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import static utilz.Constans.Direction.*;
+
+public class Keyboardinputs implements KeyListener {
+
+    private GamePanel gamePanel;
+
+    public  Keyboardinputs(GamePanel gamePanel){
+
+        this.gamePanel = gamePanel;
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()){
+            case KeyEvent.VK_W:
+                gamePanel.setDirection(UP);
+                break;
+            case KeyEvent.VK_A:
+                gamePanel.setDirection(LEFT);
+                break;
+            case KeyEvent.VK_D:
+                gamePanel.setDirection(RIGHT);
+                break;
+            case KeyEvent.VK_S:
+                gamePanel.setDirection(DOWN);
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        switch (e.getKeyCode()){
+            case KeyEvent.VK_W:
+            case KeyEvent.VK_A:
+            case KeyEvent.VK_D:
+            case KeyEvent.VK_S:
+                gamePanel.setMoving(false);
+                break;
+        }
+    }
 }
--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -1,2 +1,19 @@
-package main;public class GameWindow {
+package main;
+
+import javax.swing.*;
+import java.nio.file.FileAlreadyExistsException;
+
+public class GameWindow {
+    private JFrame Jframe;
+    public GameWindow(GamePanel gamePanel){
+
+        Jframe = new JFrame();
+
+        Jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Jframe.add(gamePanel);
+        Jframe.setLocationRelativeTo(null);
+        Jframe.setResizable(false);
+        Jframe.pack();
+        Jframe.setVisible(true);
+    }
 }
